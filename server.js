9e86a11fdbb8ca77000f1833080a46059b2ef501
Talcore <sebastian.maurer2000@gmail.com>
2018-11-09 00:28:34 +0100

    -created two interfaces (screen and control) -current song, artist and information shown

--- server.js
+++ server.js
@@ -2,19 +2,43 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(requ, res){
-  res.sendFile(__dirname + '/index.html');
+var currentsong = {song: "Example Song", artist: "Example Artist"};
+var currentinformation = "Example Information";
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/screen.html');
+});
+
+app.get('/control', function(req, res){
+  res.sendFile(__dirname + '/control.html');
 });
 
 io.on('connection', function(socket){
   console.log('Screen connected');
+
+  //send current screeninfo to new connected screen
+  socket.emit('now song changed', currentsong);
+  socket.emit('information changed', currentinformation);
+
   socket.on('disconnect', function(){
     console.log('Screen disconnected');
   });
 
+  socket.on('change now song', function(msg){
+    currentsong = msg;
+    console.log('Now Song changed to ' + msg.song + ' from ' + msg.artist);
+    io.emit('now song changed', msg);
+  });
+
   socket.on('change next song', function(msg){
-    console.log('Next Song changed to ' + msg);
+    console.log('Next Song changed to ' + msg.song + ' from ' + msg.artist);
     io.emit('next song changed', msg);
+  });
+
+  socket.on('change information', function(msg){
+    currentinformation = msg;
+    console.log('Information changed to ' + msg);
+    io.emit('information changed', msg);
   });
 });
 
